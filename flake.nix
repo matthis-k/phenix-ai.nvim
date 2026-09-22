cdbe5b1100f2a689a23823372357494a786ac9be
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-09-23 00:29:43 +0200

    chore: keep Phenix AI input tracking main

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Phenix AI Neovim client";
 
   inputs = {
-    phenix-ai.url = "github:matthis-k/phenix-ai/d7e19ea54947c65b43c1d9345c1d19972b3811a9";
+    phenix-ai.url = "github:matthis-k/phenix-ai";
     nixpkgs.follows = "phenix-ai/nixpkgs";
   };
 
